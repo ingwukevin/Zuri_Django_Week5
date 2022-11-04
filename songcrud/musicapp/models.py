eba10bf2c660bc1cd0aa67ac11f8feba8d87902d
ingwukevin <114494191+ingwukevin@users.noreply.github.com>
2022-11-04 18:22:13 +0100

    Minor changes

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -8,20 +8,18 @@ class Artiste(models.Model):
     last_name=models.CharField(max_length=20)
     age= models.IntegerField()
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 class Song(models.Model):
-    Artiste=models.ForeignKey("Artiste", on_delete=models.CASCADE)
     title= models.CharField(max_length=50)
     date_released=models.DateField(default=datetime.today)
     likes=models.PositiveIntegerField()
-    artiste_id=models.IntegerField()
+    artiste_id=models.ForeignKey("Artiste", on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return(f"{self.title}")
 
 class Lyrics(models.Model):
-    Song=models.ForeignKey("Song", on_delete=models.CASCADE)
     content=models.CharField(max_length=5000)
-    song_id=models.IntegerField()
+    song_id=models.ForeignKey("Song", on_delete=models.CASCADE)
